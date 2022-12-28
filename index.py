81c5eb0efec50719dc136a71a2fb161b0d626d0f
Drew B <dberwang@gmail.com>
2022-12-28 10:21:36 -0800

    flask app updates - added video and text results

--- index.py
+++ index.py
@@ -4,9 +4,11 @@ from flask import Flask, render_template, request, redirect, url_for, abort, sen
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.mp4']
-app.config['UPLOAD_PATH'] = 'uploads'
+app.config['UPLOAD_PATH'] = 'ByteTrack/videos'
+app.config['DOWNLOAD_PATH'] = 'ByteTrack/YOLOX_outputs/yolox_x_mix_det/track_vis'
 
 @app.errorhandler(413)
 def too_large(e):
@@ -14,27 +16,77 @@ def too_large(e):
 
 @app.route('/')
 def index():
-    files = os.listdir(app.config['UPLOAD_PATH'])
-    print(files)
-    return render_template('index.html', files=files)
+    # remove previous files in the upload path
+    for f in os.listdir(app.config['UPLOAD_PATH']):
+        os.remove(os.path.join(app.config['UPLOAD_PATH'], f))
+    
+    # remove all folder and files in the download path
+    os.system('rm -rf ' + app.config['DOWNLOAD_PATH'] + '/*')
+
+    # render the upload webpage
+    return render_template('index.html')
 
 @app.route('/', methods=['POST'])
-def upload_file():
+def upload_video():
     # Upload file
     uploaded_file = request.files['file']
     filename = secure_filename(uploaded_file.filename)
     if filename != '':
         file_ext = os.path.splitext(filename)[1]
         if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-            return "Invalid image", 400
+            return render_template('index.html')
         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-    print('file successfully uploaded')
+        
+        # Upload complete
+        return render_template('results.html')
     
-    # Process file
-    os.system('python3 ByteTrack/tools/demo_track.py video -f exps/example/mot/yolox_x_mix_det.py -c pretrained/bytetrack_x_mot17.pth.tar --fp16 --fuse --save_result')
-    print('file successfully processed.')
+    # Upload not complete, restore upload page
+    return render_template('index.html')
+
+def process_video(filename):
+    # Process video
+    os.chdir('/home/paperspace/retail-vision-analytics/ByteTrack')
+    os.system('python3 tools/demo_track.py video --path videos/' + filename + \
+        ' -f exps/example/mot/yolox_x_mix_det.py -c pretrained/bytetrack_x_mot17.pth.tar --fp16 --fuse --save_result')
+    os.chdir('/home/paperspace/retail-vision-analytics')
+
     return "File successfully uploaded and processed.", 204
 
-@app.route('/uploads/<filename>')
-def upload(filename):
-    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+@app.route('/view_mp4', methods=['GET', 'POST'])
+def view_mp4():
+    mp4_path = ''
+    mp4_filename = ''
+
+    # find the folder where of the annotated video file
+    # set mp4_path to the path of the only directory in the download path
+    for f in os.listdir(app.config['DOWNLOAD_PATH']):
+        if os.path.isdir(os.path.join(app.config['DOWNLOAD_PATH'], f)):
+            mp4_path = app.config['DOWNLOAD_PATH'] + '/' + f
+    
+    # find the annotated video file in the mp4_path
+    # it will be the only file in the mp4_path
+    mp4_filename = os.listdir(mp4_path)[0]
+
+    # check if it has the mp4 extension, fail if it does not
+    assert(os.path.splitext(os.path.join(mp4_path, mp4_filename))[1] == '.mp4')
+
+    # play the video
+    # works, but error in safari: failed to load resource: plug-in handled load
+    return send_from_directory(directory=mp4_path, filename=mp4_filename)
+
+
+@app.route('/view_txt', methods=['GET', 'POST'])
+def view_txt():
+    txt_filename = ''
+
+    # find the name of the text file
+    # it will be the only text file in the download path with the .txt extension
+    for f in os.listdir(app.config['DOWNLOAD_PATH']):
+        if os.path.splitext(os.path.join(app.config['DOWNLOAD_PATH'], f))[1] == '.txt':
+            txt_filename = f
+    
+    # display the text results
+    return send_from_directory(directory=app.config['DOWNLOAD_PATH'], filename=txt_filename)    
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- startup_script.sh
+++ startup_script.sh
@@ -5,20 +5,18 @@ sudo apt-get -y upgrade
 # install dependencies
 sudo apt install -y python3-venv
 sudo apt-get install -y protobuf-compiler libprotoc-dev
+sudo apt install python3-opencv
 
 # clone the github repository
 git clone https://github.com/ifzhang/ByteTrack.git
 cd ByteTrack
 
 # setup the virtual environment for python
-#sudo apt-get install -y python3.9-dev python3.9-venv
 sudo apt-get install -y python3.8-dev python3.8-venv
 python3 -m venv venv
 source venv/bin/activate
 
 # install the python libraries
-sudo apt install python3-opencv
-pip3 install numpy
 pip3 install -r requirements.txt
 python3 setup.py develop
 pip3 install cython
@@ -33,7 +31,3 @@ cd ..
 
 # run the demo track
 python3 tools/demo_track.py video -f exps/example/mot/yolox_x_mix_det.py -c pretrained/bytetrack_x_mot17.pth.tar --fp16 --fuse --save_result
-
-#
-# sudo apt-get install -y python3.9-dev python3.9-venv
-# sudo apt install python3-venv
